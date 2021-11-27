dc68ee46903c2ee8acb70830464b44842c39925c
nickmilanos <nickmilanos@hotmail.com>
2021-11-27 13:22:45 +0200

    reset project

--- src/UtilityFunctions.js
+++
@@ -1,6 +0,0 @@
-export const moveToTop = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-};
-
-export const scrollDown = (target) => document.querySelector(target).scrollIntoView();
