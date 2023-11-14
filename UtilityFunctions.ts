c20c98e8bbfe41a35bcb9c389693cb15eaa513df
nickmilanos <nickmilanos@hotmail.com>
2023-11-14 19:11:19 +0200

    minor

--- UtilityFunctions.ts
+++ UtilityFunctions.ts
@@ -1,6 +1,7 @@
 export const moveToTop = (): void => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 };
 
-export const scrollDown = (target: string): void => document.querySelector(target).scrollIntoView();
+export const scrollDown = (target: string): void =>
+  document.querySelector(target).scrollIntoView();
